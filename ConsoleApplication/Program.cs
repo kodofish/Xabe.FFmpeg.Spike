20abf079d065915f24bcad556bac6d459cd91e4c
STACKCORE\yuwei.chang <yuwei.chang@stackcore.com>
2023-11-15 11:55:16 +0800

    Remove static video file, add video download functionality
    The commit removes the bundled 'SampleVideo_1280x720_1mb.mp4' file from the application and instead adds a feature to download a video file over the network.
    Previously, the application used a local video file ('SampleVideo_1280x720_1mb.mp4') that had been included in the project's resources for testing video conversion. This has been removed.
    Now, a function RunDownloadVideo has been added that downloads a video from the provided URL. The file path preparation and check for file existence are included in the new function. This change allows better flexibility in testing different video files and reduces the bundled application size by not including static files.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -24,7 +24,9 @@ namespace ConsoleApplication
 
             Console.WriteLine(FFmpeg.ExecutablesPath);
 
-            var file = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Demo", "SampleVideo_1280x720_1mb.mp4"));
+            // [Download Sample Videos / Dummy Videos For Demo Use](https://sample-videos.com/)
+            // 這個網站有許多影片可以下載, 用來做測試
+            var file = RunDownloadVideo("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_10mb.mp4").GetAwaiter().GetResult();
 
             RunConversion(file).GetAwaiter().GetResult();
 
@@ -32,12 +34,42 @@ namespace ConsoleApplication
 
             Console.ReadLine();
         }
-
-        private static string outputFileNameBuilder(string number)
+        
+        /// <summary>
+        /// 傳入一個 mp4 檔案的 url, 將這個檔案下載到本地端
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private static async Task<FileInfo> RunDownloadVideo(string url)
         {
-            return "fileNameNo" + number + ".png";
-        }
+            //先建立一個資料夾, 用來存放下載的影片檔案
+            var directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Demo");
 
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            //使用傳入的 url 來解析檔案名稱, 並且將檔案名稱加到資料夾路徑後面
+            var file = new FileInfo(Path.Combine(directory, Path.GetFileName(url)));
+
+            //如果檔案已經存在, 就先刪除
+            if (file.Exists)
+                file.Delete();
+            
+            //使用 httpClient 來下載檔案
+            
+            using (var httpClient = new System.Net.WebClient())
+            {
+                //使用 GetByteArrayAsync 方法, 將檔案下載到指定路徑
+                await httpClient.DownloadFileTaskAsync(url, file.FullName);
+            }
+
+            //印出下載完成的訊息
+            Console.WriteLine($"{file.FullName} file downloaded.");
+
+            return file;
+        }
 
         //實作一個方法, 傳入 FileInfo 型別的參數, FileInfo 會是 Mp4 影片檔, 要從這個影片擷取封面圖檔
         private static async Task RunGetThumbnail(FileInfo file)
@@ -67,7 +99,7 @@ namespace ConsoleApplication
         ///     將影片轉成 webm 格式
         /// </summary>
         /// <param name="file"></param>
-        private static async Task RunConversion(FileInfo file)
+        private static async Task RunConversion(FileSystemInfo file)
         {
             var outputFileName = Path.ChangeExtension(file.FullName, ".webm");
 
